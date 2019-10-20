9218a24e1dc9e4f19974a422fd29969bddb7bbab
epsilon-2048 <1099204713@qq.com>
2019-10-20 21:56:51 +0800

    增加hystrix服务监控

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -19,7 +19,10 @@ public class DeptController {
     //全局异常与服务熔断？
     //全局异常处理的是服务抛出的异常，而服务熔断处理的是服务宕机
     //服务降级是指资源紧张，暂时将某些非核心服务主动停掉，空出资源给核心服务，当资源不紧张时再启动非核心服务
+    //所谓降级，一般是从整体负荷考虑，就是当某个服务熔断之后，服务器不再调用，
+    //此时客户端可以自己准备一个本地的fallback回调，返回一个缺省值。这样做，虽然服务水平下降，但好歹可用，比直接挂掉要强
     @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    //服务监控好像基于HystrixCommand的，我在feign使用fallback图形化监控里无数据，显示loading
     @HystrixCommand(fallbackMethod = "processHystrix_Get")//这种方式会有两个问题1：方法膨胀，每个接口都会有对应的fallbackMethod，2这里其实是切面，将切面（processHystrix_Get）与业务逻辑整合在一起，导致高耦合
     public Dept get( @PathVariable("id") Long id){
         Dept dept = deptService.getDept(id);
